5fef3e2a1ce7c05da6c7c63577f4fa06db2184ed
macarenaoyague <macarena.oyague@utec.edu.pe>
2022-06-20 21:52:01 -0500

    fix: moffat con paper ieee

--- Algorithms/MoffatAndTakaoka.hpp
+++ Algorithms/MoffatAndTakaoka.hpp
@@ -3,6 +3,7 @@
 #include "Spira.hpp"
 
 #include <cmath>
+#include <algorithm>
 
 class MoffatAndTakaoka : public BaseAlgorithm {
 private:
@@ -89,4 +90,95 @@ public:
         delete spira;
         delete dantzig;
     }
+};
+
+class MoffatAndTakaokaIEEE : public Algorithm<arrayType>{
+private:
+    unordered_set<vertexIndex> purged;
+
+    Edge* getEdgeCandidate(vertexIndex s) override {
+        // linked list optimizacion
+        Edge* edgeUseful = nullptr;
+        auto vertex = this->graph->findVertex(s);
+        auto edges = vertex->edges;
+        bool isPurgued = false;
+        while (!isPurgued) {
+            size_t i = (*currentUsefulEdge)[s];
+            if(i != edges.size()) edgeUseful = edges[i++];
+            (*currentUsefulEdge)[s] = i;
+        }
+        return edgeUseful;
+    }
+
+    void insertCandidate(Edge* candidate) override {
+        auto weight = (*D)[candidate->start] + candidate->weight;
+        candidateEdges->push_back({weight, candidate}); 
+        push_heap(candidateEdges->begin(), candidateEdges->end());
+    }
+
+    Edge* getCandidateOfLeastWeight() override {
+        assert(candidateEdges->size() != 0);
+        Edge* top = (candidateEdges->front()).second;
+        pop_heap(candidateEdges->begin(), candidateEdges->end());
+        candidateEdges->pop_back();
+        return top;
+    }
+    
+    unordered_map<vertexIndex, weightType>* algorithmExpand(size_t limit) override {
+        vertexIndex c, t;
+        weightType weight;  // C(c, t)
+        while(S->size() < limit) {
+            this->initializeValues(c, t, weight, this->getCandidateOfLeastWeight());
+            addEdgeCandidate(c);
+            if (this->S->find(t) == this->S->end()) {
+                this->S->emplace(t);
+                (*D)[t] = (*D)[c] + weight;
+                addEdgeCandidate(t);
+            }
+        }
+    }
+
+    void cleanUpHeap() {
+        purged = *S;
+        erase_if(*(this->candidateEdges), [this](auto& item) -> bool{
+            vertexIndex c, t;
+            weightType weight;
+            this->initializeValues(c, t, weight, item.second);
+            if(S->find(t) != S->end()){
+                auto edge = getEdgeCandidate(c);
+                if(edge){
+                    item.second = edge;
+                    item.first = this->getDvector()[edge->start] + edge->weight;
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        });
+        make_heap(this->candidateEdges->begin(), this->candidateEdges->end());
+    }
+
+    void fastSp(vertexIndex s) {
+        assert(this->graph->findVertex(s) != nullptr);
+        S->emplace(s);
+        (*D)[s] = 0;
+        purged.emplace(s);
+        size_t n = graph->getNumberOfVertices();
+        for (size_t k = 1; k < log2(log2(n)); k++) {
+            // n div 2**k
+            this->algorithmExpand(n - n/(pow(2, k)));
+            this->cleanUpHeap();
+        }
+        this->algorithmExpand(n);
+    }
+
+public:
+    MoffatAndTakaokaIEEE(Graph* _graph) : Algorithm<arrayType>(_graph) {
+
+    }
+
+    unordered_map<vertexIndex, weightType>* getDvector() {
+        return this->D;
+    }
+
 };
